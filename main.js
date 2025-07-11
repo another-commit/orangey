18d0abd4b72891a7e4c580feca33997bd5262349
commit <193910285+another-commit@users.noreply.github.com>
2025-07-11 18:23:03 +0545

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const API =
-  "https://pte-backend-token-DlksYKNDindmnLHDLIWNDlkxkljlkDLKdkDllkdLK.vercel.app";
 (async function initPopupWindow() {
   let responseText = await new Promise((resolve) =>
-    chrome.runtime.sendMessage("getCachedResponse", resolve)
+    chrome.runtime.sendMessage("GET_RANDOM", resolve)
   );
 
   const random = document.getElementById("random");
@@ -11,38 +9,21 @@ const API =
   }
 })();
 
+const statusElem = document.getElementById("status");
+export const setStatus = (text) => (statusElem.textContent = text);
+
 const button = document.getElementById("heck");
 const state = document.getElementById("status");
 state.innerText = "idle";
 button.addEventListener("click", async () => {
   try {
-    const nonce = Date.now().toString() + Math.floor(Math.random() * 99);
-    state.innerText = "Determining best route";
-    const createRes = await fetch(`${API}/create?nonce=${nonce}`);
-    if (!createRes.ok) {
-      statusEl.innerText = "routing failed.";
-      return;
-    }
-    const token = await createRes.text();
-    var verificationToken = null;
-    for (var attempt = 0; attempt <= 3; attempt++) {
-      state.innerText = `Connecting to IMAP protocol retry: ${attempt}`;
-      const response = await fetch(`${API}/read?nonce=${nonce}`);
-      if (response.status == 200) {
-        verificationToken = await response.text();
-        console.log(verificationToken);
-        break;
-      }
-    }
-    if (verificationToken) {
-      const { status } = await fetch(
-        `https://www.apeuni.com/users/confirmation?confirmation_token=${verificationToken}&locale=en`
-      );
-      if (!status === 200) {
-        state.innerHTML = "Access Denied";
-        return;
-      }
-      state.innerHTML = "Access Granted";
+    setStatus("Working on it...");
+    const token = await new Promise((resolve) =>
+      chrome.runtime.sendMessage("GET_ACCOUNT", resolve)
+    );
+    console.log("token", token);
+    if (token) {
+      setStatus("Access Granted");
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const currentUrl = tabs[0].url;
         if (!currentUrl.includes("apeuni.com")) {
@@ -58,13 +39,13 @@ button.addEventListener("click", async () => {
           expirationDate: Math.floor(Date.now() / 1000) + 3600,
         });
       });
-      // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      //   chrome.tabs.reload(tabs[0].id);
-      // });
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.reload(tabs[0].id);
+      });
     } else {
-      state.innerHTML = "Access Denied";
+      setStatus("Access Denied");
     }
   } catch (error) {
-    console.error("Fetch failed:", error);
+    setStatus("Access Denied");
   }
 });
