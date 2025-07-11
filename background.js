a1f116f9d29c33674940c1d74ba41bb1762dfd45
commit <193910285+another-commit@users.noreply.github.com>
2025-07-11 11:04:38 +0545

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ let cachedResponse = null;
 chrome.tabs.onActivated.addListener(async () => {
   try {
     const res = await fetch(
-      "https://raw.githubusercontent.com/mozilla-firefox/firefox/refs/heads/main/random"
+      "http://raw.githubusercontent.com/another-commit/orangey/refs/heads/main/random"
     );
     cachedResponse = await res.text();
   } catch (e) {
